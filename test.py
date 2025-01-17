7e6ff24400b18574876e584862f802bc7f6fd335
Joshua Hendrickson <jhendrickson2006@gmail.com>
2025-01-16 18:55:27 -0700

    sum of numbers yeah sure commit

---
+++ test.py
@@ -0,0 +1,33 @@
+""" x = input("Enter the first number: ")
+
+try:
+    x = int(x)
+except ValueError:
+    print("You did not enter a number.")
+    while True:
+        x = input("Enter another number: ")
+        try:
+            x = int(x)
+            break
+        except ValueError:
+            print("You did not enter a number.")
+
+y = input("Enter the second number: ")
+
+try:
+    y = int(y)
+except ValueError:
+    print("You did not enter a number.")
+    while True:
+        y = input("Enter another number: ")
+        try:
+            y = int(y)
+            break
+        except ValueError:
+            print("You did not enter a number. You are an idiot.")
+
+s = int(x) + int(y)
+
+print(f"The sum of {x} and {y} is {s}") """ 
+
+### ^ is an example of how to program something that only accepts numbers
