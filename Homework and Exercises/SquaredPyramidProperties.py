f9de5fde09e1aac1213178b416a60708ea3f8789
Joshua Hendrickson <jhendrickson2006@gmail.com>
2025-01-29 17:56:00 -0700

    im a coding genius

---
+++ Homework and Exercises/SquaredPyramidProperties.py
@@ -0,0 +1,17 @@
+"""
+This is an example of how to program a program that calculates the properties of a squared pyramid.
+Authur: Joshua Hendrickson with the help of Copilot. 
+"""
+
+print("This program will calculate the properties of a squared pyramid.")
+basel = input("Please enter the base length: ")
+h = input("Please enter the height: ")
+
+length = float(basel)
+height = float(h)
+
+sa = (length ** 2) + (2 * length) * ((((length ** 2) / 4) + (height ** 2)) ** 0.5)
+volume = (length ** 2) * (height / 3)
+
+print(f"The surface area of this squared pyramid is {sa} squared inches!")
+print(f"The volume of this squared pyramid is {volume} cubic inches!")
