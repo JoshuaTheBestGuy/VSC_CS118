2c9f206ec715c875c4e962f5b6bf0fef5dfbddba
Joshua Hendrickson <jhendrickson2006@gmail.com>
2025-02-05 15:46:58 -0700

    i hate python

--- Homework_and_Exercises/Exercise18.py
+++ Homework_and_Exercises/Exercise18.py
@@ -1,4 +1,17 @@
-def do_more_things(a, b):
-    a = "hello"
-    b = 1
-    print("A IS", a, "B IS", b)
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+def print_none():
+    print("I got nothin.")
+
+print_two("Zed","Shaw")
+print_two_again("Zed","Shaw")
+print_one("First!")
+print_none()
