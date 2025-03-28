c17af9925b585648a6a303d053e50d1356537d61
Joshua Hendrickson <jhendrickson2006@gmail.com>
2025-03-28 15:42:56 -0700

    wow it works now im so good

--- perfectroll.py
+++ perfectroll.py
@@ -8,6 +8,7 @@ import time
 start_time = time.perf_counter()
 my_list = []
 average_time_list = []
+average_attempt_list = []
 current_counter = 1
 max_roll_counter = 0
 attempts = 1
@@ -64,15 +65,26 @@ while runs_completed < runs_input:
                 if do_print is True:
                     print(f"You rolled {dice}! That is not a perfect combination! You got up to {max_roll_counter}... so close.. or not? The highest you got to was {highest_counter}.")
                 attempts += 1
-                my_list = list()
+                my_list = []
                 current_counter = 1
                 max_roll_counter = 0
-    runs_completed += 1
     end_time_run = time.perf_counter()
     elapsed_time_perrun = end_time_run - start_time_run
     average_time_list.append(elapsed_time_perrun)
-    print(f"It took {attempts} attempts to get a perfect combination of rolls. It took {elapsed_time_perrun:.4f} seconds.")
+    average_attempt_list.append(attempts)
+    if do_print is True:
+        print(f"It took {attempts} attempts to get a perfect combination of rolls. It took {elapsed_time_perrun:.4f} seconds.")
+    current_counter = 1
+    max_roll_counter = 0
+    my_list = []
+    attempts = 1
+    highest_counter = 0
+    runs_completed += 1
 
 end_time = time.perf_counter()
 elapsed_time = end_time - start_time
-print(f"{runs_completed} completed. It took {elapsed_time:.4f} seconds total. The average time for each run was {sum(average_time_list) / len(average_time_list)}")
+average_time_list.sort()
+average_attempt_list.sort()
+print(f"\n{runs_completed} runs completed. It took '{elapsed_time:.4f}' seconds total. The average time for each run was '{sum(average_time_list) / len(average_time_list)}' The average attempt took '{int(sum(average_attempt_list) / len(average_attempt_list))}' attempts to get a perfect roll of up to {number}.")
+print(f"The quickest attempt took '{average_time_list[0]}' while the longest attempt took '{average_time_list[-1]}'.")
+print(f"The least attempts taken in a run was '{average_attempt_list[0]}' while the most attempts taken was '{average_attempt_list[-1]}'.\n")
