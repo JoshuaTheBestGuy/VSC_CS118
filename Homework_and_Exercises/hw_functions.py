f99d349b7308379cdec6808dc497e2e55a5772a9
Joshua Hendrickson <jhendrickson2006@gmail.com>
2025-03-31 16:15:33 -0700

    changed name

---
+++ Homework_and_Exercises/hw_functions.py
@@ -0,0 +1,12 @@
+"""
+This program will define two functions for other uses. 
+Author: Joshua Hendrickson
+"""
+
+def mode(x:list) -> tuple:
+    """ """
+    print("wow")
+
+def rev_lookup(d:dict, v:int) -> tuple:
+    """ """
+    print("wow")
